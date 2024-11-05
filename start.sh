d2d76d92d828ea18b5bf56b5940c42c2e2f854c6
Domen Hocevar <domenhocevar1@gmail.com>
2024-11-05 16:13:10 +0000

    Add milvus startup to start script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 docker network create network
-# docker compose -f ./milvus2/docker-compose.yaml up
-docker run -p 7200:7200 --name graphdb --network network ontotext/graphdb:10.6.2 &
-docker run -p 3000:3000 --name client --network network client &
-docker run -p 5000:5000 --name server --network network server &
+bash ./milvus2/standalone_embed_latest.sh start
+docker run -p 5002:7200 --name graphdb --network network ontotext/graphdb:10.6.2 &
+docker run -p 5001:3000 --name client --network network client &
+docker run -p 5003:5000 -p 5004:8000 --name server --network network server &
